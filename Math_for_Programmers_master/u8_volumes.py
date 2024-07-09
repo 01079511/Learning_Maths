ae177a4ce9209a139f737e865da367cc025cc385
01079511 <164169216+01079511@users.noreply.github.com>
2024-07-09 14:39:41 +0800

    2024/07/09 u8.2

--- Math_for_Programmers_master/u8_volumes.py
+++ Math_for_Programmers_master/u8_volumes.py
@@ -61,6 +61,18 @@ def flow_rate(t):
     return 3*(t-4)**2 / 64
 
 
+def decreasing_volume(t):
+    """
+    绘制了油箱中石油体积随时间递减的曲线
+    :param t:时间
+    :return:随时间递减后的体积
+    """
+    if t < 5:
+        return 10 - (t**2)/5
+    else:
+        return 0.2*(10-t)**2
+
+
 def average_flow_rate(v, t1, t2):
     """
     根据体积计算平均流速
@@ -87,7 +99,7 @@ def secant_line(f, x1, x2):
 
 def plot_secant(f, x1, x2, color='k'):
     """
-    # 结合secant_line,实现两个给定点之间绘制函数f的割线
+    结合secant_line,实现两个给定点之间绘制函数f的割线
     :param f: 用于接受函数
     :param x1:
     :param x2:
@@ -99,15 +111,68 @@ def plot_secant(f, x1, x2, color='k'):
     plt.scatter([x1, x2], [f(x1), f(x2)], c=color)  # 绘制散点图
 
 
+def interval_flow_rates(v, t1, t2, dt):
+    """
+    计算不同时间段内的平均流速，来近似得到流速随时间变化的函数(实例10小时内按小时分段)
+    :param v:体积函数
+    :param t1:开始时间
+    :param t2:结束时间
+    :param dt:间隔
+    :return:包括时间和流速对的列表
+    """
+    # 对于每一个间隔的开始时间 t，找到从t到t+dt的平均流速（我们要的是t及其对应速率的列表）
+    return [(t, average_flow_rate(v, t, t+dt)) for t in np.arange(t1, t2, dt)]
+
+
+def plot_interval_flow_rates(volume, t1, t2, dt):
+    """
+    利用scatter 函数来快速绘制表示这些流速随时间变化的图
+    :param v:体积函数
+    :param t1:开始时间
+    :param t2:结束时间
+    :param dt:间隔
+    :return:将结果传给scatter函数
+    """
+    series = interval_flow_rates(volume, t1, t2, dt)
+    times = [t for (t, _) in series]
+    rates = [q for (_, q) in series]
+    plt.scatter(times, rates)
+
+
+def linear_volume_function(t):
+    """
+    实现linear_volume_function函数，并画出流速随时间的变化图，以说明流速是恒定的
+    数学形式是： V(t) = at + b, 其中a和b为常数
+    :param t:
+    :return:
+    """
+    return 5 * t + 3
+
+
 print('volume(4)={0:.2f},\n'
       'volume(9)={1:.2f},\n'
-      'average_flow_rate(volume,4,9)={2:.2f}'.format(volume(4),
+      'average_flow_rate(volume,4,9)={2:.2f}\n'.format(volume(4),
                                                  volume(9),
                                                  average_flow_rate(volume, 4, 9)))
-# 体积随时间变化曲线
-plot_volume(volume, 0, 10)
+# 打印interval_flow_rates(v, t1, t2, dt)
+# print(interval_flow_rates(volume, 0, 10, 1))
+
+# 体积随时间变化曲线,递增
+# plot_volume(volume, 0, 10)
+# 体积随时间变化曲线,递减
+# plot_volume(decreasing_volume, 0, 10)
+# flow_rate 函数表示的是任意时间点上流速的瞬时值。
+# plot_flow_rate(flow_rate, 0, 10)
 # 割线
-plot_secant(volume, 4, 8)
+# plot_secant(volume, 4, 8)
+
+# interval_flow_rates所产生数据的散点图
+# plot_interval_flow_rates(volume, 0, 10, 1)
+# 绘制decreasing_volume 函数流速随时间的变化图。什么时候其流速为最小值？也就是说，石油什么时候离开油箱的速度最快？
+# plot_interval_flow_rates(decreasing_volume, 0, 10, 0.5)
+# linear_volume_function函数,画出流速随时间的变化图,体现流速是恒定的 ==> 线性体积函数,流速不随时间变化
+plot_interval_flow_rates(linear_volume_function, 0, 10, 0.25)
+
 # 画图
 plt.show()
 plt.close()
